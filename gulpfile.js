d144a172a18facbddea5caaa371f70cd89cc9868
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-11-01 12:32:43 +1300

    revamped gulpfile.js with modules. added .jscsrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,94 @@
+var path = require('path');
 var gulp = require('gulp');
-var gls = require('gulp-live-server');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var runSequence = require('run-sequence');
+var nodemon = require('gulp-nodemon');
+var plumber = require('gulp-plumber');
+var server = require('tiny-lr')();
 
-gulp.task('default', function() {
-    var server = gls('bin/www');
-    server.start().then(function(result) {
-        console.log('Server exited with result:', result);
-        process.exit(result.code);
-    });
-    gulp.watch(['public/**/*.css', 'public/**/*.html'], function(file) {
-        server.notify.apply(server, [file]);
-    });
-    gulp.watch('bin/www', server.start);
+// *** config *** //
+
+var paths = {
+  scripts: [
+    path.join('routes', '**', '*.js')
+  ],
+  styles: [
+    path.join('public', 'stylesheets', 'css', '**', '*.css')
+  ],
+  views: [
+    path.join('views', '**', '*.pug')
+  ],
+  server: 'bin/www'
+};
+
+var lrPort = 35729;
+
+const nodemonConfig = {
+  script: paths.server,
+  ext: 'html js css',
+  ignore: ['node_modules'],
+  env: {
+    NODE_ENV: 'development'
+  }
+};
+
+
+//default task
+gulp.task('default', () => {
+  runSequence(
+    ['jshint'],
+    ['jscs'],
+    ['lr'],
+    ['nodemon'],
+    ['watch']
+  );
 });
 
-gulp.task('static', function() {
-    var server = gls.static('static', 3000);
-    server.start();
-    gulp.watch(['public/**/*.css', 'public/**/*.html'], function(file) {
-        server.notify.apply(server, [file]);
-    });
+//sub tasks
+
+gulp.task('jshint', () => {
+  return gulp.src(paths.scripts)
+    .pipe(plumber())
+    .pipe(jshint({
+      esnext: true
+    }))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('lint', function() {
-  return gulp.src('routes/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', { verbose: true }));
+gulp.task('jscs', () => {
+  return gulp.src(paths.scripts)
+    .pipe(plumber())
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(jscs.reporter('fail'));
+});
+
+gulp.task('styles', () => {
+  return gulp.src(paths.styles)
+    .pipe(plumber());
+});
+
+gulp.task('views', () => {
+  return gulp.src(paths.views)
+    .pipe(plumber());
+});
+
+gulp.task('lr', () => {
+  server.listen(lrPort, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+});
+
+gulp.task('nodemon', () => {
+  return nodemon(nodemonConfig);
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.scripts, ['jshint', 'jscs']);
+  gulp.watch(paths.styles, ['styles']);
 });
