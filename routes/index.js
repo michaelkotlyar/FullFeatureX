413cd13bb0145fb22ea124d2b8537d4c02b5b39d
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-11-01 23:51:37 +1300

    best polishes 2017

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dashboard' });
+});
+
+/////////////////////////
+/* Aesthetic redirects */
+/////////////////////////
+
+router.get('/dashboard', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/login', function(req, res, next) {
+  res.redirect('/users/login');
 });
 
 module.exports = router;
