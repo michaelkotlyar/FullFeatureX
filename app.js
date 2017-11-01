49d727e2d2998f315393cb6c546172870d8e847b
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-11-01 14:47:03 +1300

    tweaked path cnvetion n app.js

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  root: path.join(__dirname, 'public/stylesheets'),
+  root: path.join(__dirname, 'public', 'stylesheets'),
   src: 'sass',
   dest: 'css',
   prefix: '/stylesheets/css',
