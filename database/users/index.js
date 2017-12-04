636982f29628aaf1ee20493858f093bb88e5caee
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-12-05 11:49:01 +1300

    testing function

--- database/users/index.js
+++ database/users/index.js
@@ -3,7 +3,26 @@ var db = require('/database');
 var allUsers = function() {
   let sco;
 
-  db.query('SELECT * FROM users').then(() => {
-    return
-  })
+  db.connect()
+    .then(obj => {
+        // obj.client = new connected Client object;
+
+        sco = obj; // save the connection object;
+
+        // execute all the queries you need:
+        return sco.any('SELECT * FROM Users');
+    })
+    .then(data => {
+        // success
+        return data;
+    })
+    .catch(error => {
+        // error
+    })
+    .finally(() => {
+        // release the connection, if it was successful:
+        if (sco) {
+            sco.done();
+        }
+    });
 }
