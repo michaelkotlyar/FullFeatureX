2d9d8133fdede483b4e885423049046dbb02b854
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-12-15 16:04:24 +1300

    added method that uses database and exports that. this makes users directory redundant

--- database/index.js
+++ database/index.js
@@ -1,11 +1,24 @@
-var pgp = require('pg-promise')(/*options*/);
-
-var cn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
+const initOptions = {
+  error(err, e) {
+    console.log(e);
+    console.log(err);
+  }
 };
 
-module.exports = pgp(cn);
+const pgp = require('pg-promise')(initOptions);
+
+const cn = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+
+const db = pgp(cn);
+
+const getAllUsers = () => db.any('SELECT * FROM users');
+
+module.exports = {
+  getAllUsers
+};
