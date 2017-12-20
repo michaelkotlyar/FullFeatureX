07cc5506c7a1831aa41e1e8f026d936465cbf7e3
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-12-21 10:30:00 +1300

    made helper module eport more direct

--- config/helpers.js
+++ config/helpers.js
@@ -3,35 +3,35 @@ var bcrypt = require('bcryptjs');
 
 var salt = bcrypt.genSaltSync(10);
 
-function loggedIn(req, res, next) {
-  if (!req.user) {
-    res.redirect('/login');
-  }
-  else {
-    next();
-  }
-}
-
-function createUser(req, res) {
-  renderObject = { title: "Register" };
-  db.uniqueUserUsername(req.body.username)
+module.exports = {
+  loggedIn: (req, res, next) => {
+    if (!req.user) {
+      res.redirect('/login');
+    }
+    else {
+      next();
+    }
+  },
+  createUser: (req, res) => {
+    renderObject = { title: "Register" };
+    db.uniqueUserUsername(req.body.username)
     .then(data => {
       if (!data[0].exists) {
         db.uniqueUserEmail(req.body.email)
-          .then(data => {
-            if (!data[0].exists) {
-              var hash = bcrypt.hashSync(req.body.password, salt);
-              db.registerUser(req.body.username, hash, req.body.email);
-              res.redirect('/login');
-            }
-            else {
-              renderObject.warning = 'This email is already taken.';
-              res.render('register', renderObject);
-            }
-          })
-          .catch(error => {
-            console.log('error: ' + error);
-          });
+        .then(data => {
+          if (!data[0].exists) {
+            var hash = bcrypt.hashSync(req.body.password, salt);
+            db.registerUser(req.body.username, hash, req.body.email);
+            res.redirect('/login');
+          }
+          else {
+            renderObject.warning = 'This email is already taken.';
+            res.render('register', renderObject);
+          }
+        })
+        .catch(error => {
+          console.log('error: ' + error);
+        });
       }
       else {
         renderObject.warning = 'This username is already taken.';
@@ -41,14 +41,9 @@ function createUser(req, res) {
     .catch(error => {
       console.log('error: ' + error);
     });
-}
-
-function modifyUser(req, res) {
-  var changeUsername = false, changeEmail = false, changeImage = false;
-  if (req.username !== req.body.username && req.username !== '') changeUsername = true;
-}
-
-module.exports = {
-  loggedIn,
-  createUser
+  },
+  modifyUser: (req, res) => {
+    var changeUsername = false, changeEmail = false, changeImage = false;
+    if (req.username !== req.body.username && req.username !== '') changeUsername = true;
+  }
 };
