b0359fedb69d2d50a06aa67234f96e8d358fdf94
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-12-21 15:58:42 +1300

    moved mocha and chai to devdependencies, added login tests but has revealed hole in database setup (WARNING: Creating a duplicate QueryFile object for the same file)

--- test/test.js
+++ test/test.js
@@ -1,9 +1,40 @@
-var assert = require('assert');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+chai.use(chaiHttp);
+
+describe('Logging In', function() {
+  describe('Borat Logs In', function() {
+    it('should log Borat into the app with the correct password', function(done) {
+      chai.request(server)
+        .post('/login')
+        .send({
+          'username': 'Borat',
+          'password': 'borat'
+        })
+        .end(function(err, res) {
+          res.status.should.equal(200);
+          res.redirects.length.should.equal(2);
+          done();
+        });
+    });
+  });
+
+  describe('Borat Logs In With Wrong Password', function() {
+    it('should not log Borat into the app with the wrong password', function(done) {
+      chai.request(server)
+        .post('/login')
+        .send({
+          'username': 'Borat',
+          'password': 'notborat'
+        })
+        .end(function(err, res) {
+          res.status.should.equal(200);
+          res.redirects.length.should.equal(1);
+          done();
+        });
     });
   });
 });
