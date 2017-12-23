8bbb5e901934200459ef3a5e46b2aee73e8ad095
uzimike <kotlyamich@ecs.vuw.ac.nz>
2017-12-23 16:50:12 +1300

    utilised parameters in express.static method

--- server/app.js
+++ server/app.js
@@ -29,9 +29,9 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 // front end modules
-app.use('/vendor/jquery', express.static(path.join(__dirname, '..', '/node_modules/jquery/dist')));
-app.use('/vendor/popper.js', express.static(path.join(__dirname, '..', '/node_modules/popper.js/dist')));
-app.use('/vendor/bootstrap', express.static(path.join(__dirname, '..', '/node_modules/bootstrap/dist')));
+app.use('/vendor/jquery', express.static(path.join(__dirname, '..', 'node_modules', 'jquery', 'dist')));
+app.use('/vendor/popper.js', express.static(path.join(__dirname, '..', 'node_modules', 'popper.js', 'dist')));
+app.use('/vendor/bootstrap', express.static(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist')));
 
 //session / passport
 app.use(session({
