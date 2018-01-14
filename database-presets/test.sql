4aa64c14d2e8ada82a41242689562b6afe9e7fcb
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-01-15 11:27:40 +1300

    made seperate database for testing

---
+++ database-presets/test.sql
@@ -0,0 +1,27 @@
+create table user_types (
+  type_code integer unique not null,
+  type_name varchar(20) unique not null
+);
+
+COPY user_types (type_code, type_name) FROM stdin;
+0	user
+1	admin
+\.
+
+create table users (
+  user_id serial primary key,
+  user_name varchar(25) not null,
+  user_password varchar(100) not null,
+  user_email varchar(100) unique not null,
+  user_type integer references user_types(type_code) default 0
+);
+
+-- Create users (Username: Borat, Password: borat) (Username: Michael, Password: michael)
+COPY users (user_name, user_password, user_email) FROM stdin;
+Borat	$2a$10$JFLMr12qjfoTdVOp/INmvOCKzzs0r0KvMQQX9O4aOSnrEIC79Xvoi	borat@kazakhstan.com
+Michael	$2a$10$2st4h76DMLOhU4flJPcqA.70vWFTpaCzPJ/dKQrtMli6IWg2UaFCC	michael@michael.com
+\.
+
+-- Add and admin account
+INSERT INTO users(user_name, user_password, user_email, user_type)
+VALUES('Admin', '$2a$10$ulPQqSOKLDFf.L58H1SIGuo3giVkvq2P3YlvL8Y2kG93IUxlmEsg2', 'admin@admin.com', 1);
--- server/config/database.js
+++ server/config/database.js
@@ -6,10 +6,16 @@ var initOptions = {
 
 var pgp = require('pg-promise')(initOptions);
 
+var test = '';
+
+if (process.env.NODE_ENV === 'test') {
+  test = '_test';
+}
+
 var cn = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME + test,
   user: process.env.DB_USER,
   password: process.env.DB_PASS
 };
