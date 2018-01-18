c7e1a92c3f8d4e9e12dffef3a350e4bd26142371
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-01-18 22:39:24 +1300

    i have to manually hash them here it seems

--- src/server/config/database/seeds/02_users.js
+++ src/server/config/database/seeds/02_users.js
@@ -1,34 +1,33 @@
-var bcrypt = require('bcryptjs');
 var faker = require('faker');
-var User = require('../../models/user');
+var bcrypt = require('bcryptjs');
 
-function profiles() {
-  var profiles = [
+function hash(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+function users() {
+  var usersArray = [
     {username: 'Michael', hash: hash('michael'), email: 'michael@fullfeaturex.com'},
     {username: 'Borat', hash: hash('borat'), email: 'borat@fullfeaturex.com'},
     {username: 'Admin', hash: hash('admin'), email: 'admin@fullfeaturex.com', type: 1}
   ];
   if (process.env.NODE_ENV == 'development') {
     for (var i = 0; i < 100; i++) {
-      profiles.push({
+      usersArray.push({
         username: faker.internet.userName(),
         hash: hash(faker.internet.password()),
         email:faker.internet.email()
       });
     }
   }
-  return profiles;
-}
-
-function hash(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
+  return usersArray;
+};
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert(profiles()); // Adds in the profiles
+      return knex('users').insert(users()); // Adds in the users
     });
 };
