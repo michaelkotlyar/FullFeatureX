aefa1ed3307e78f7cce28dab6e21ee6962307982
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-01-18 22:39:50 +1300

    i dont need bcrypt for user types

--- src/server/config/database/seeds/01_usertypes.js
+++ src/server/config/database/seeds/01_usertypes.js
@@ -1,9 +1,3 @@
-var bcrypt = require('bcryptjs');
-
-function hash(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('user_types').del()
