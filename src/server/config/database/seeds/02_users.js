eee1180c18aeb6bd496146ac7730acb0fa86eeb9
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-01-18 18:03:34 +1300

    implemented faker profiles

--- src/server/config/database/seeds/02_users.js
+++ src/server/config/database/seeds/02_users.js
@@ -1,4 +1,24 @@
 var bcrypt = require('bcryptjs');
+var faker = require('faker');
+var User = require('../../models/user');
+
+function profiles() {
+  var profiles = [
+    {username: 'Michael', hash: hash('michael'), email: 'michael@fullfeaturex.com'},
+    {username: 'Borat', hash: hash('borat'), email: 'borat@fullfeaturex.com'},
+    {username: 'Admin', hash: hash('admin'), email: 'admin@fullfeaturex.com', type: 1}
+  ];
+  if (process.env.NODE_ENV == 'development') {
+    for (var i = 0; i < 100; i++) {
+      profiles.push({
+        username: faker.internet.userName(),
+        hash: hash(faker.internet.password()),
+        email:faker.internet.email()
+      });
+    }
+  }
+  return profiles;
+}
 
 function hash(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
@@ -9,10 +29,6 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        {username: 'Michael', hash: hash('michael'), email: 'michael@fullfeaturex.com'},
-        {username: 'Borat', hash: hash('borat'), email: 'borat@fullfeaturex.com'},
-        {username: 'Admin', hash: hash('admin'), email: 'admin@fullfeaturex.com', type: 1}
-      ]);
+      return knex('users').insert(profiles()); // Adds in the profiles
     });
 };
