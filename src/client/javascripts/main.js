49846f7965b8ca2df2df6fcb5aa82c3a3329cbe0
uzimike <kotlyamich@ecs.vuw.ac.nz>
2018-01-22 11:01:41 +1300

    implemented confirm password

--- src/client/javascripts/main.js
+++ src/client/javascripts/main.js
@@ -3,7 +3,16 @@ $(function() {
   const PATH = window.location.pathname;
 
   if (PATH === '/users/register') {
-    var form = $('form#form-register');
+    $('#registerPassword, #registerConfirmPassword').on('keyup', function () {
+      if ($('#registerPassword').val() === $('#registerConfirmPassword').val()) {
+        $('#registerConfirmPassword').removeClass('is-invalid');
+        $('#registerConfirmPassword').addClass('is-valid');
+      }
+      else {
+        $('#registerConfirmPassword').removeClass('is-valid');
+        $('#registerConfirmPassword').addClass('is-invalid');
+      }
+    });
   }
 
 });
